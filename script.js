566901b1ee91f33b2968bff9578dac434cb48440
Anukul Das <anukul449@gmail.com>
2024-09-18 19:32:21 +0530

    my first commit in this project

---
+++ script.js
@@ -0,0 +1,12 @@
+let main = document.querySelector("#main")
+let crsr = document.querySelector(".cursor")
+
+
+// crsr.style.backgroundColor = "green"
+
+main.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+
+    
+})
